286a8e70ad2d61a1fe8be9f357ee5393939acee2
shanME <giorgie103188@gmail.com>
2024-09-29 18:43:52 +0400

    slider

--- src/components/Slide/Slide.js
+++ src/components/Slide/Slide.js
@@ -1,9 +1,40 @@
 import React from "react";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import Slider from "react-slick";
+import Sdata from "./Sdata";
+import "./style.css";
 
 const Slide = () => {
+  let settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    arrows: false,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+  };
+
   return (
     <>
-      <h1>Slide</h1>
+      <div className="slide">
+        <Slider {...settings}>
+          {Sdata.map((value) => {
+            return (
+              <div className="box">
+                <div className="img">
+                  <img src={value.cover} alt="img" />
+                </div>
+                <div className="text">
+                  <span>{value.category}</span>
+                  <h2>{value.title}</h2>
+                  <p>{value.desc}</p>
+                </div>
+              </div>
+            );
+          })}
+        </Slider>
+      </div>
     </>
   );
 };
